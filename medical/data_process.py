9f09a7652f535c0621b4d69487cc333d499f3ead
DRZQB <191771508@qq.com>
2021-09-09 13:26:49 +0800

    file add

--- medical/data_process.py
+++ medical/data_process.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from OtherUtils import load_vocab
 from tqdm import tqdm
 import numpy as np
+import json
 
 
 def medical(filepath, train_tfrecordfilepath, dev_tfrecordfilepath):
@@ -79,8 +80,123 @@ def medical(filepath, train_tfrecordfilepath, dev_tfrecordfilepath):
     print("验证集样本数：", m_samples_dev)
 
 
+def bi2bieso():
+    """
+    BI标注转BIESO标注
+    :return:
+    """
+    fw = open("data/OriginalFiles/train_bieso.txt", "w", encoding="utf-8")
+
+    lastchar = "-2"
+    lastlabel = "-2"
+
+    with open("data/OriginalFiles/train.txt", "r", encoding="utf-8") as fr:
+        for line in fr:
+            linenew = line.rstrip()
+
+            if linenew == "":
+                fw.write(lastchar + "\t" + lastlabel + "\n")
+                lastchar = "-1"
+                lastlabel = "-1"
+
+            else:
+                linenew = linenew.split("\t")
+                char = linenew[0]
+                label = linenew[1]
+                if label != "O":
+                    if lastlabel == "-2":
+                        pass
+                    elif lastlabel == "-1":
+                        fw.write("\n")
+                    elif lastlabel == "O":
+                        fw.write(lastchar + "\t" + lastlabel + "\n")
+                    else:
+                        label_prefix, label_suffix = label.split("-")
+                        lastlabel_prefix, lastlabel_suffix = lastlabel.split("-")
+                        if lastlabel_suffix == "B":
+                            if label_suffix == "B":
+                                fw.write(lastchar + "\t" + lastlabel_prefix + "-S" + "\n")
+                            else:
+                                fw.write(lastchar + "\t" + lastlabel + "\n")
+                        else:
+                            if label_suffix == "B":
+                                fw.write(lastchar + "\t" + lastlabel_prefix + "-E" + "\n")
+                            else:
+                                fw.write(lastchar + "\t" + lastlabel + "\n")
+                else:
+                    if lastlabel == "-2":
+                        pass
+                    elif lastlabel == "-1":
+                        fw.write("\n")
+                    elif lastlabel == "O":
+                        fw.write(lastchar + "\t" + lastlabel + "\n")
+                    else:
+                        lastlabel_prefix, lastlabel_suffix = lastlabel.split("-")
+                        if lastlabel_suffix == "B":
+                            fw.write(lastchar + "\t" + lastlabel_prefix + "-S" + "\n")
+                        else:
+                            fw.write(lastchar + "\t" + lastlabel_prefix + "-E" + "\n")
+
+                lastchar = char
+                lastlabel = label
+
+    fw.close()
+
+
+def bieso2span():
+    all_datas = []
+
+    k = 0
+    startid = 0
+    context = ""
+    span_posLabel = {}
+    data = {}
+
+    with open("data/OriginalFiles/train_bieso.txt", "r", encoding="utf-8") as fr:
+        for line in fr:
+            line = line.rstrip()
+
+            if line == "":
+                data["context"] = context
+                data["span_posLabel"] = span_posLabel
+                all_datas.append(data)
+
+                context = ""
+                span_posLabel = {}
+                data = {}
+                k = 0
+            else:
+                char, label = line.split("\t")
+
+                if label == "O":
+                    pass
+                else:
+                    label_prefix, label_suffix = label.split("-")
+
+                    if label_suffix == "B":
+                        startid = k
+
+                    elif label_suffix == "E":
+                        endid = k
+                        span_posLabel[str(startid) + ";" + str(endid)] = label_prefix
+
+                    elif label_suffix == "S":
+                        span_posLabel[str(k) + ";" + str(k)] = label_prefix
+
+                context += char
+
+                k += 1
+
+    with open("data/OriginalFiles/train_span.txt", "w", encoding="utf-8") as f:
+        json.dump(all_datas, f, ensure_ascii=False, indent=2)
+
+
 if __name__ == "__main__":
-    medical("data/OriginalFiles/train.txt",
-            "data/TFRecordFiles/train.tfrecord",  # 7036
-            "data/TFRecordFiles/dev.tfrecord",  # 781
-            )
+    # medical("data/OriginalFiles/train.txt",
+    #         "data/TFRecordFiles/train.tfrecord",  # 7036
+    #         "data/TFRecordFiles/dev.tfrecord",  # 781
+    #         )
+
+    # bi2bieso()
+
+    bieso2span()
